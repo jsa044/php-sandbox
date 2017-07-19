b4cd04c5fadeda9c4e79b984a03526743f797641
Jeff Allaire <jeff.allaire@gmail.com>
2017-07-19 17:17:31 -0500

    add new files and changes

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 <html>
 	<head>
 		<title>Welcome to your Web App</title>
+		<link rel="stylesheet" type='text/css' href='assets/css/style.css'>
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet"> 
 	</head>
 	
 	<body>
+		<header>
+		   <a href="index.php">Intentional Energy</a>
+		</header>   
+		   <h1>Register</h1>
 		<h1>Please Login or Register</h1>
-		
+		<a href='login.php'>Login</a> or
+		<a href='register.php'>Register</a>
 	</body>
 	
 	
